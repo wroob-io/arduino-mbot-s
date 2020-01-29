a686663a02bf429105e6bf59e928e0e47a6b315b
Rudy <oleszczyk.m@gmail.com>
2020-01-29 18:25:54 +0100

    refactor: change program concept to be more RAM efficient

--- robot_obibok.ino
+++ robot_obibok.ino
@@ -1,20 +1,6 @@
 #include <MeMCore.h>
 #include <WroobImp.h>
 
-#define MOTORS F("motors")
-#define SENSORS F("sensors")
-#define MOTOR1 F("motor1")
-#define MOTOR2 F("motor2")
-#define US0 F("US0")
-#define VALUES F("values")
-#define EV F("ev")
-#define CMD F("cmd")
-#define SET_SPEED   F("SetSpeed")
-#define GET_SPEED   F("GetSpeed")
-#define GET_RANGE   F("GetRange")
-#define SET_CONTENT F("SetLedDispBmp")
-#define COLUMNS     F("cols")
-
 MeDCMotor motor1(M1);
 MeDCMotor motor2(M2);
 MeUltrasonicSensor us0(PORT_3);
@@ -24,38 +10,54 @@ WroobImp wroob("MBSM");
 int speed1 = 0;
 int speed2 = 0;
 unsigned char drawBuffer[16] = {0,112,136,142,137,113,1,1,1,1,113,137,142,136,112,0};
+const char *motor1_str = "motor1";
+const char *motor2_str = "motor2";
+const char *motors_str = "motors";
+const char *sensors_str = "sensors";
+const char *values_str = "values";
+const char *us0_str = "US0";
+const char *cols_str = "cols";
+const char *cmd_str = "cmd";
+const char *ev_str = "ev";
+const char *set_speed_str = "SetSpeed";
+const char *get_speed_str = "GetSpeed";
+const char *get_range_str = "GetRange";
+const char *set_led_str = "SetLedDispBmp";
 
 void my_callback(JsonObject &payload) {
   StaticJsonDocument<128> event;
 
-  if (payload[CMD] == SET_SPEED) {
-    if (payload[VALUES].containsKey(MOTOR1)) {
-      motor1.run(payload[VALUES][MOTOR1]);
-      speed1 = payload[VALUES][MOTOR1];
+  if (payload[cmd_str].isNull())
+    return;
+      
+  if (strcmp(payload[cmd_str], set_speed_str) == 0) {  
+    if (payload[values_str].containsKey(motor1_str)) {
+      motor1.run(payload[values_str][motor1_str]);
+      speed1 = payload[values_str][motor1_str];
     }
-    if (payload[VALUES].containsKey(MOTOR2)) {
-      motor2.run(payload[VALUES][MOTOR2]);
-      speed2 = payload[VALUES][MOTOR2];
+    if (payload[values_str].containsKey(motor2_str)) {
+      motor2.run(payload[values_str][motor2_str]);
+      speed2 = payload[values_str][motor2_str];
     }
-  } else if (payload[CMD] == GET_SPEED) {
-    event[EV] = GET_SPEED;
-    for (int i = 0; i < payload[MOTORS].size(); i++) {
-      if (payload[MOTORS].getElement(i) == MOTOR1)
-        event[VALUES][MOTOR1] = speed1;
-      if (payload[MOTORS].getElement(i) == MOTOR2)
-        event[VALUES][MOTOR2] = speed2;
-    }
-    wroob.sendMessage(event);
-  } else if (payload[CMD] == GET_RANGE) {
-    event[EV] = GET_RANGE;
-    for (int i = 0; i < payload[SENSORS].size(); i++) {
-      if (payload[SENSORS].getElement(i) == US0)
-        event[VALUES][US0] = us0.distanceCm();
+  } else if (strcmp(payload[cmd_str], get_speed_str) == 0) {
+    event[ev_str] = get_speed_str;
+    for (int i = 0; i < payload[motors_str].size(); i++) {
+      if (payload[motors_str].getElement(i) == motor1_str)
+        event[values_str][motor1_str] = speed1;
+      if (payload[motors_str].getElement(i) == motor2_str)
+        event[values_str][motor2_str] = speed2;
     }
     wroob.sendMessage(event);
-  } else if (payload[CMD] == SET_CONTENT) {
+  } else if (strcmp(payload[cmd_str], get_range_str) == 0) {
+    event[ev_str] = get_range_str;
+    for (int i = 0; i < payload[sensors_str].size(); i++) {
+      if (payload[sensors_str].getElement(i) == us0_str)
+        event[values_str][us0_str] = us0.distanceCm();
+    }
+    wroob.sendMessage(event);
+  } else if (strcmp(payload[cmd_str], set_led_str) == 0) {
     ledMtx.clearScreen();
-    copyArray(payload[COLUMNS], drawBuffer, payload[COLUMNS].size());
+    copyArray(payload[cols_str], drawBuffer, payload[cols_str].size());
     ledMtx.drawBitmap(0, 0, 16, drawBuffer);
   }
 }
